0d02b4dcc94ab5a559bf457d4c6a7fa573a4b09d
shaninalex <shanin.x@gmail.com>
2023-09-06 13:57:43 +0300

    feat: workgin on authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { NotesModule } from './notes/notes.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -15,6 +16,7 @@ import { NotesModule } from './notes/notes.module';
         DatabaseModule,
         UsersModule,
         NotesModule,
+        AuthModule,
    ],
     controllers: [AppController],
     providers: [AppService],
---
+++ src/auth/constants.ts
@@ -0,0 +1,6 @@
+export const jwtConstants = {
+
+    // get from environment variables
+    secret: 'B8j6rzbuxkj24wZ2uEFaOwvXfPnd7B4zkyLhQRn93bvR/sGiF506YiMEtkmfq4asFVEUTVcpAn9aS7gBRYbHYQ'
+};
+
