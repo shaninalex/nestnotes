925d3f6e1e08c07bcedb5a8ded90ae1bd6c16fdf
shaninalex <shanin.x@gmail.com>
2023-09-12 18:35:55 +0300

    feat: hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../models/user.entity';
 import { UserService } from '../user/user.service';
+import * as argon2 from "argon2";
 
 
 @Injectable()
@@ -13,12 +14,9 @@ export class AuthService {
 
     async login(email: string, pass: string): Promise<any> {
         const user: User = await this.userService.findOne(email);
-
-        // TODO: add hashing
-        if (user?.password !== pass) {
+        if (!await argon2.verify(user.password, pass)) {
             throw new UnauthorizedException();
         }
-
         const payload = { sub: user.id, email: user.email };
         return {
             access_token: await this.jwtService.signAsync(payload)
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../models/user.entity';
+import * as argon2 from "argon2";
+
 
 @Injectable()
 export class UserService {
@@ -11,6 +13,8 @@ export class UserService {
     ) { }
 
     async create(user: Partial<User>): Promise<User> {
+        const hash = await argon2.hash(user.password);
+        user.password = hash;
         const newuser = this.userRepository.create(user);
         return this.userRepository.save(newuser);
     }
