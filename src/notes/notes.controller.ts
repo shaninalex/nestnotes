c73855e7b6b512629bbbb3f92a4cc65515705529
shaninalex <shanin.x@gmail.com>
2023-09-14 17:21:54 +0300

    feat: patch note

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Patch, Param } from '@nestjs/common';
 import { Note } from 'src/models/note.entity';
 import { NotesService } from './notes.service';
 import { Request } from 'express';
+import { UpdateResult } from 'typeorm';
 
 
 @Controller('notes')
@@ -13,7 +14,6 @@ export class NotesController {
 
     @Post()
     create(@Body() payload: Note, @Req() request: Request): Promise<Note> {
-        // TODO: Get user id from cookie
         payload.user = request["sub"];
         const newNote = this.noteService.create(payload);
         return newNote;
@@ -21,7 +21,19 @@ export class NotesController {
 
     @Get()
     async allNotes(@Req() request: Request): Promise<Note[]> {
-        console.log(request["sub"]);
-        return [];
+        const notes = this.noteService.get(request["sub"]);
+        return notes;
+    }
+
+    @Patch('/:note_id')
+    async updateNote(
+        @Param() params: any,
+        @Body() payload: Note, 
+        @Req() request: Request
+    ): Promise<UpdateResult> {
+        payload.user = request["sub"];
+        payload.id = params.note_id;
+        const result = this.noteService.patch(request["sub"], payload);
+        return result
     }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, Repository, UpdateResult } from 'typeorm';
 import { Note } from 'src/models/note.entity';
+import { of } from 'rxjs';
 
 @Injectable()
 export class NotesService {
@@ -13,5 +14,15 @@ export class NotesService {
     async create(note: Partial<Note>): Promise<Note> {
         const newNote = this.noteRepository.create(note);
         return this.noteRepository.save(newNote);
+    }
+
+    async get(user_id: number): Promise<Note[]> {
+        const where: FindOptionsWhere<Note> = {"user": user_id}; 
+        return this.noteRepository.findBy(where);
+    }
+
+    async patch(user_id: number, payload: Partial<Note>): Promise<UpdateResult> {
+        const where: FindOptionsWhere<Note> = {"id": payload.id, "user": user_id};
+        return this.noteRepository.update(where, payload);
     }
 }
